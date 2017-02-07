3cc07975a1140075c5412c5a509bad5b1730a62f
Hari Yadav <yadav.hari@gmail.com>
2017-02-06 20:57:33 -0800

    writing more tests

--- src/test/java/xyz/cloudblog/account/AccountServiceTest.java
+++ src/test/java/xyz/cloudblog/account/AccountServiceTest.java
@@ -6,20 +6,40 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import xyz.cloudblog.account.bean.Account;
+
 public class AccountServiceTest {
 
-	
+	private AccountService aService;
+	private Account account;
 	@Before
 	public void setUp() throws Exception {
+		aService = new AccountServiceImpl();
+		account = new Account(12344L, 2134.235);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		aService = null;
 	}
 
 	@Test
-	public void test() {
-		assertEquals("abc", "abc");
+	public void testCheckBalance() {
+		Double aBalance = aService.checkBalance(account);
+		assertEquals(account.getBalance(), aBalance);
 	}
 
+	@Test
+	public void testDeposit(){
+		Double newBalance = aService.deposit(account, 100d);
+		Double expected = account.getBalance() + 100d;
+		assertEquals(expected, newBalance); 
+	}
+	
+	@Test
+	public void testWithdrawal(){
+		Double newBalance = aService.withdraw(account, 100d);
+		Double expected = account.getBalance() - 100d;
+		assertEquals(expected, newBalance); 
+	}
 }
