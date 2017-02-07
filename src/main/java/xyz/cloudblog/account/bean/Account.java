a0d2d3200615649501a741ff783e8ab1bf6d8d4a
Hari Yadav <yadav.hari@gmail.com>
2017-02-06 21:00:29 -0800

    added more properties

--- src/main/java/xyz/cloudblog/account/bean/Account.java
+++ src/main/java/xyz/cloudblog/account/bean/Account.java
@@ -1,9 +1,16 @@
 package xyz.cloudblog.account.bean;
 
 public class Account {
-	
-	private Long accountID;
 
+	private Long accountID;
+	private Double balance;
+
+	public Account() {	}
+	
+	public Account(Long acountID, Double balance){
+		this.accountID = acountID;
+		this.balance = balance;
+	}
 	public Long getAccountID() {
 		return accountID;
 	}
@@ -11,7 +18,13 @@ public class Account {
 	public void setAccountID(Long accountID) {
 		this.accountID = accountID;
 	}
-	
-	
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
 
 }
